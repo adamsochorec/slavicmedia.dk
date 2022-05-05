1366339ac8d5fd50b05df1bab6bcb384be30401b
Adam S <98590550+adamsochorec@users.noreply.github.com>
2022-05-05 13:53:57 +0200

    upade content

--- js/style.js
+++ js/style.js
@@ -47,6 +47,40 @@ function getPassword(){
 
 
 
+// 2FA numbers
+
+// 1.
+(function () {
+	let tfaCode = ["483 130","583 059","574 028","190 459","869 248","294 905","103 059" ],
+	i = -1;
+	setInterval(function(){ $('.tfaCode').fadeOut(function(){
+	  $(this).html(tfaCode[(i = (i + 1) % tfaCode.length)]).fadeIn();
+	  }); }, 1000)
+	})();
+	//2.
+	(function () {
+	let tfaCodee = ["483 130","583 059","574 028","190 459","869 248","294 905","103 059" ],
+	i = -1;
+	setInterval(function(){ $('.tfaCodee').fadeOut(function(){
+	  $(this).html(tfaCodee[(i = (i + 1) % tfaCodee.length)]).fadeIn();
+	  }); }, 1500)
+	})();
+	// 3.
+	(function () {
+	let tfaCodeee = ["483 130","583 059","574 028","190 459","869 248","294 905","103 059" ],
+	i = -1;
+	setInterval(function(){ $('.tfaCodeee').fadeOut(function(){
+	  $(this).html(tfaCodeee[(i = (i + 1) % tfaCodeee.length)]).fadeIn();
+	  }); }, 1000)
+	})();
+	// 4.
+	(function () {
+	let tfaCodeeee = ["483 130","583 059","574 028","190 459","869 248","294 905","103 059" ],
+	i = -1;
+	setInterval(function(){ $('.tfaCodeeee').fadeOut(function(){
+	  $(this).html(tfaCodeeee[(i = (i + 1) % tfaCodeeee.length)]).fadeIn();
+	  }); }, 1500)
+	})();
 	
 
 
