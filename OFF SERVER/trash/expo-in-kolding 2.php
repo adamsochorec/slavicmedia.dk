fa0c84c4ccb93cd63005856df618a57fd102b794
Adam S <98590550+adamsochorec@users.noreply.github.com>
2023-04-11 13:20:44 +0200

    snad už

---
+++ OFF SERVER/trash/expo-in-kolding 2.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta
+      name="description"
+      content="I have the great pleasure to invite you to the first official exposition of my works!
+The expo is going to be in inner spaces of the Knud's Garage in Kolding.
+You can take a look on my on large scale prints framed in historical frames series, on which I was working on last spring."
+    />
+    <meta
+      name="keywords"
+      content="Portfolio, Works, Photography, Photos, 3D, 3D scans, Animations, Art, exposition, expo, exhibition, knud's garage, kolding, denmark, danmark, syddanmark, kommune, Munkegade, Adam Sochorec, @adamsochorec, Rostislav Sochorec, Gabriela Sochorcová, Sofie Sochorcová, Petra Sochorcová, Magdalena Sochorcová"
+    />
+    <title>Expo in Kolding | Adam Sochorec</title>
+    <?php
+    $IPATH = $_SERVER['DOCUMENT_ROOT'] . '/assets/php/';
+    include $IPATH . 'global-head.php';
+    ?>
+  </head>
+
+  <body>
+    <?php
+    $IPATH = $_SERVER['DOCUMENT_ROOT'] . '/assets/php/';
+    include $IPATH . 'global-nav-bar.php';
+    ?>
+    <main id="expo-in-kolding">
+      <article class="intro-section">
+        <section class="intro-subsection">
+          <section class="flex-center" id="countdown">
+            <ul>
+              <li><span id="days"></span>days</li>
+              <li><span id="hours"></span>Hours</li>
+              <li><span id="minutes"></span>Minutes</li>
+            </ul>
+          </section>
+          <br /><br />
+          <section class="read-more">
+            <a class="flex-center" href="#scrolllink">
+              <h5>Learn more about the event</h5>
+              <lottie-player
+                id="scrollLottie"
+                src="/img/lottie/scroll-down.json"
+                background="transparent"
+                loop
+                speed="1"
+                autoplay
+              >
+              </lottie-player>
+            </a>
+          </section>
+        </section>
+        <div class="blur"></div>
+      </article>
+      <article id="scrolllink" class="wrapper-standard">
+        <div class="pathname-container reveal">
+          <i
+            ><span class="pathname"><a href="/"></a> / </span>
+            <span class="pathname-current"></span
+          ></i>
+        </div>
+        <br />
+        <section>
+          <h1 class="reveal"></h1>
+          <br />
+          <p class="reveal">
+            I have the great pleasure to invite you to the first official
+            exposition of my works!<br />
+            The expo is going to be in inner spaces of the
+            <a
+              href="https://knuds-garage.business.site/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Knud's Garage</a
+            >
+            in Kolding.<br />You can take a look on my on large scale prints
+            framed in historical frames series, on which I was working during
+            the last spring.
+          </p>
+          <hr />
+        </section>
+        <section class="reveal">
+          <section class="expo grid-container">
+            <section class="expo grid-item">
+              <div class="panel">
+                <h3>Where</h3>
+                <p>
+                  <b>Knud's Garage</b><br />Munkegade 5A<br />Kolding 6000<br />Denmark
+                  🇩🇰
+                </p>
+              </div>
+            </section>
+            <section class="expo grid-item">
+              <div class="panel">
+                <h3>When</h3>
+                <p>
+                  <b>Jan - Feb 2023</b><br /><br />Mon -
+                  Sat&ensp;&emsp;12.00-02.00<br />Sun&emsp;&emsp;&emsp;&emsp;&nbsp;14.00-22.00
+                </p>
+              </div>
+            </section>
+            <br /><br />
+          </section>
+          <div class="btn-area flex-center">
+            <a
+              aria-describedby="about"
+              href="https://www.google.com/maps/dir//Knuds+Garage,+Munkegade+5A,+6000+Kolding,+Denmark/@55.4896193,9.4774543,17z/data=!4m15!1m6!3m5!1s0x0:0x4beb056a0a3fdde7!2sKnuds+Garage!8m2!3d55.4896141!4d9.4775472!4m7!1m0!1m5!1m1!1s0x464c9f7aed9ccbcd:0x4beb056a0a3fdde7!2m2!1d9.4775472!2d55.4896141?hl=en"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div aria-describedby="about" id="btn">Get the directions</div>
+              <div class="btn-shadow"></div>
+            </a>
+          </div>
+        </section>
+        <hr />
+      </article>
+    </main>
+    <?php
+    $IPATH = $_SERVER['DOCUMENT_ROOT'] . '/assets/php/';
+    include $IPATH . 'global-footer.php';
+    ?>
+  </body>
+</html>
---
+++ OFF SERVER/trash/trash/tube-collector 2.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php
+    $IPATH = $_SERVER['DOCUMENT_ROOT'] . '/assets/php/';
+    include $IPATH . 'blog-desc.php';
+    ?>
+    <meta name="keywords" content="" />
+    <title>Tube collector | Adam Sochorec</title>
+    <meta name="robots" content="noindex" />
+    <meta name="googlebot" content="noindex" />
+    <?php
+    $IPATH = $_SERVER['DOCUMENT_ROOT'] . '/assets/php/';
+    include $IPATH . 'global-head.php';
+    ?>
+  </head>
+
+  <body id="tube-collector" class="blog-item">
+    <?php
+    $IPATH = $_SERVER['DOCUMENT_ROOT'] . '/assets/php/';
+    include $IPATH . 'trash-nav-bar.php';
+    ?>
+    <main>
+      <article class="wrapper-standard">
+        <div class="pathname-container">
+          <i
+            ><span class="pathname"><a href="/"></a> / </span>
+            <span class="pathname projects"><a href="/#projects"></a> / </span>
+            <span class="pathname-current">Tube collector</span></i
+          >
+        </div>
+        <br />
+        <h1>Tube collector</h1>
+        <iframe
+          title="Blåvand bunker | vzor 2"
+          frameborder="0"
+          allowfullscreen
+          mozallowfullscreen="true"
+          webkitallowfullscreen="true"
+          allow="autoplay; fullscreen; xr-spatial-tracking"
+          xr-spatial-tracking
+          execution-while-out-of-viewport
+          execution-while-not-rendered
+          web-share
+          src="https://sketchfab.com/models/dd16bb89f8844760ab31396d495fd0ce/embed?autospin=1&ui_theme=dark&dnt=1"
+          ><p class="alt-alt">3D scan of an Atlantic Wall bunker from WWII.</p>
+        </iframe>
+        <p>
+          We are excited to introduce our new product, the Tube Collector, to
+          your city. The Tube Collector is a revolutionary solution for
+          optimizing the recycling and can collection process.
+        </p>
+
+        <hr />
+        <h2>Product features:</h2>
+        <br />
+        <ul>
+          <li>
+            <p>
+              One Tube Collector can accommodate four 300 ml cans, which means
+              that if two are installed in each trash bin, the capacity for cans
+              would increase by 2.6 times compared to the current solution,
+              which can only hold three cans. Additionally, this will free up
+              space in the trash bins as people will not need to crush cans
+              before throwing them away.
+            </p>
+          </li>
+          <li>
+            <p>
+              The Tube Collector does not require any modifications to the
+              current trash bins in the city, as it can be easily screwed into
+              existing holes. This means that it can be implemented quickly and
+              without any disruption to the current system.
+            </p>
+          </li>
+          <li>
+            <p>
+              People who rely on collecting cans for income will benefit greatly
+              from the Tube Collector, as they will no longer have to sift
+              through garbage to find cans. Furthermore, the number of cans
+              returned for recycling will increase as cans will no longer be
+              crushed and devalued during trash collection.
+            </p>
+          </li>
+          <li>
+            <p>
+              Emptying the Tube Collector is easy and convenient. Simply place a
+              plastic bag underneath and open the valve. Additionally, it will
+              simplify and optimize the recycling process at the recycling
+              center, as there will be no need for additional steps to separate
+              cans from other trash.
+            </p>
+          </li>
+          <li>
+            <p>
+              The Tube Collector's design is not intrusive and will fit
+              seamlessly into the urban architecture. We believe that the Tube
+              Collector will be a valuable addition to your city's efforts to
+              promote recycling and reduce waste.
+            </p>
+          </li>
+        </ul>
+        <section class="img">
+          <img src="/img/jpg/IMG_4370.jpg" alt="#" />
+          <p>
+            <i class="note"
+              >Pant collectors would avoid unhygienic rubbish bin search</i
+            >
+          </p>
+        </section>
+        <p>
+          We look forward to the opportunity to work with you to implement the
+          Tube Collector in Kolding. Don't heistate to
+          <a href="/#contact">contact us</a> in case in any questions<br /><br />Sincerely,<br />
+          <i>Sympaticka Prostituka a.s.</i>
+        </p>
+        <?php
+          $IPATH = $_SERVER['DOCUMENT_ROOT'] . '/assets/php/';
+          include $IPATH . 'global-totop.php';
+          ?>
+      </article>
+    </main>
+    <?php
+    $IPATH = $_SERVER['DOCUMENT_ROOT'] . '/assets/php/';
+    include $IPATH . 'global-footer.php';
+    ?>
+  </body>
+</html>
