ac1a41c27e83de1f5fe51cddd0af2e492ba6c889
Adam S <98590550+adamsochorec@users.noreply.github.com>
2023-05-10 17:13:21 +0200

    BLUR IS BACK

--- assets/php/global-nav-bar.php
+++ assets/php/global-nav-bar.php
@@ -1,21 +1,20 @@
 <header>
-  <div class="container">
-    <nav id="navigation">
-      <a href="/"
-        ><img class="logo" alt="Slavic Media logo" src="/img/logo.png"
-      /></a>
-      <a aria-label="mobile menu" class="hamburger">
-        <span></span>
-        <span></span> <span></span>
-      </a>
-      <ul class="menu-left">
-        <!--   <li><a href="/#services">Services</a></li>-->
-        <li><a href="/#about">About</a></li>
-        <li><a href="/#book">Book</a></li>
-      </ul>
-    </nav>
+  <div class="blur">
+    <div class="container">
+      <nav id="navigation">
+        <a href="/"
+          ><img class="logo" alt="Slavic Media logo" src="/img/logo.png"
+        /></a>
+        <a aria-label="mobile menu" class="hamburger">
+          <span></span>
+          <span></span> <span></span>
+        </a>
+        <ul class="menu-left">
+          <!--   <li><a href="/#services">Services</a></li>-->
+          <li><a href="/#about">About</a></li>
+          <li><a href="/#book">Book</a></li>
+        </ul>
+      </nav>
+    </div>
   </div>
-  <div class="blur master-blur"></div>
-
-  <!-- NAVIGATION BAR END -->
 </header>
