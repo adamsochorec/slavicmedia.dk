9074d0f518a779d6bd8dbfe2310f8579bafacfcf
Adam S <98590550+adamsochorec@users.noreply.github.com>
2023-06-07 19:12:22 +0200

    youtube setup

--- success.php
+++ success.php
@@ -30,18 +30,7 @@
         <h1>Form submitted<br />successfully!</h1>
         <br />
         <h2>
-          <a href="/"
-            >Return to the homepage
-            <svg
-              id="chevron-right"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 384 512"
-            >
-              <path
-                d="M342.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L274.7 256 105.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"
-              />
-            </svg>
-          </a>
+          <a href="/">Return to the homepage &#8250; </a>
         </h2>
         <hr />
       </article>
