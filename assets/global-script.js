0e5d12e9b39489990b866865e4a36d8df2545c81
Adam S <98590550+adamsochorec@users.noreply.github.com>
2023-11-14 21:07:32 +0100

    script optimised again

--- assets/global-script.js
+++ assets/global-script.js
@@ -1,4 +1,19 @@
-// LOADER ANIMATION START
+// CONTENT REVEAL START
+function reveal() {
+  const reveals = document.querySelectorAll(".reveal"),
+    windowHeight = window.innerHeight;
+
+  reveals.forEach((reveal) => {
+    const revealtop = reveal.getBoundingClientRect().top,
+      revealpoint = 0;
+    if (revealtop < windowHeight - revealpoint) {
+      reveal.classList.add("active");
+    }
+  });
+}
+window.addEventListener("scroll", reveal);
+reveal();
+// CONTENT REVEAL END// LOADER ANIMATION START
 (function introLoader() {
   window.addEventListener("load", () => {
     // When the window is fully loaded, hide the loader
@@ -6,7 +21,67 @@
     loader.style.display = "none";
   });
 })();
-// LOADER ANIMATION END
+// LOADER ANIMATION END// RECAPTCHA START
+function onSubmit(token) {
+  document.getElementById("contactForm").submit();
+}
+// RECAPTCHA END
+// VIMEO GALLERY START
+$(document).ready(function () {
+  $(".video-gallery").magnificPopup({
+    delegate: "a",
+    type: "iframe",
+    gallery: {
+      enabled: true,
+    },
+  });
+});
+// VIMEO GALLERY END
+// SWIPER START
+if (document.body.id === "servicees") {
+  const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    // And if we need scrollbar
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+  });
+  window.swiperNested1 = new Swiper("swiper-nested-1", {
+    mode: "horizontal",
+    pagination: ".pagination",
+    paginationClickable: true,
+    preventLinks: true,
+    resistance: "100%",
+    slidesPerView: 1,
+    onSlideClick: video,
+  });
+  function video() {
+    if (!clicked) {
+      playVideo();
+      console.log("Slide clicked 1st");
+      window.clicked = true;
+    } else if ((clicked = 2)) {
+      player.pauseVideo();
+      console.log("Slide clicked 2nd");
+      window.clicked = false;
+    } else {
+      resumeVideo();
+      window.clicked = 2;
+    }
+  }
+}
+// SWIPER END
 
 (function header() {
   let lastScrollTop = 0;
@@ -61,106 +136,36 @@
   }
 })();
 
-// SWIPER START
-if (document.body.id === "servicees") {
-  const swiper = new Swiper(".swiper", {
-    // Optional parameters
-    direction: "horizontal",
-    loop: true,
-    // If we need pagination
-    pagination: {
-      el: ".swiper-pagination",
-    },
-
-    // Navigation arrows
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    // And if we need scrollbar
-    scrollbar: {
-      el: ".swiper-scrollbar",
-    },
-  });
-}
-// SWIPER END
-// CONTENT REVEAL START
-function reveal() {
-  const reveals = document.querySelectorAll(".reveal"),
-    windowHeight = window.innerHeight;
-
-  reveals.forEach((reveal) => {
-    const revealtop = reveal.getBoundingClientRect().top,
-      revealpoint = 0;
-    if (revealtop < windowHeight - revealpoint) {
-      reveal.classList.add("active");
-    }
-  });
-}
-window.addEventListener("scroll", reveal);
-reveal();
-// CONTENT REVEAL END
 // HOMEPAGE START
-// CHANGE HEADER RGBA AT SCROLL START
-document.addEventListener("DOMContentLoaded", function () {
-  window.addEventListener("scroll", function () {
-    const header = document.querySelector("#homepage header");
-    (blur = document.querySelector(".blur")),
-      (blurValue = getComputedStyle(document.documentElement)
-        .getPropertyValue("--blur-2")
-        .trim());
-
-    if (window.pageYOffset > 500) {
-      header.style.backgroundColor = "rgba(0, 0, 0, 0.2)";
-      blur.style.backdropFilter = blurValue;
-      blur.style.webkitBackdropFilter = blurValue;
-      blur.style.MozBackdropFilter = blurValue; // Unsupported
-      blur.style.MsBackdropFilter = blurValue; // Unsupported
-      blur.style.OBackdropFilter = blurValue; // Unsupported
-    } else {
-      header.style.backgroundColor = "rgba(0, 0, 0, 0)";
-      blur.style.backdropFilter = "blur(0px)";
-      blur.style.webkitBackdropFilter = "blur(0px)";
-      blur.style.MozBackdropFilter = "blur(0px)"; // Unsupported
-      blur.style.MsBackdropFilter = "blur(0px)"; // Unsupported
-      blur.style.OBackdropFilter = "blur(0px)"; // Unsupported
-    }
-  });
-
-  function createLottieInteractivity(playerId) {
-    LottieInteractivity.create({
-      player: playerId,
-      mode: "scroll",
-      actions: [
-        {
-          visibility: [0.1, 1.0],
-          type: "play",
-        },
-      ],
-    });
-  }
-  createLottieInteractivity("#photoLottie");
-  createLottieInteractivity("#videoLottie");
-  createLottieInteractivity("#graphicdesignLottie");
-});
 
 // CHANGE HEADER RGBA AT SCROLL END
 if (document.body.id === "homepage") {
-  // SKILL BARS START
-  function setProgress(e, progress) {
-    e.style.opacity = 1;
-    e.style.width = `${progress}%`;
-  }
-  function showProgress() {
-    const skillBars = document.querySelectorAll(".skill-bar");
+  // CHANGE HEADER RGBA AT SCROLL START
+  document.addEventListener("DOMContentLoaded", function () {
+    window.addEventListener("scroll", function () {
+      const header = document.querySelector("#homepage header");
+      (blur = document.querySelector(".blur")),
+        (blurValue = getComputedStyle(document.documentElement)
+          .getPropertyValue("--blur-2")
+          .trim());
 
-    skillBars.forEach((bar) => {
-      const progress = bar.dataset.progress;
-      setProgress(bar, progress);
+      if (window.pageYOffset > 680) {
+        header.style.backgroundColor = "rgba(0, 0, 0, 0.2)";
+        blur.style.backdropFilter = blurValue;
+        blur.style.webkitBackdropFilter = blurValue;
+        blur.style.MozBackdropFilter = blurValue; // Unsupported
+        blur.style.MsBackdropFilter = blurValue; // Unsupported
+        blur.style.OBackdropFilter = blurValue; // Unsupported
+      } else {
+        header.style.backgroundColor = "rgba(0, 0, 0, 0)";
+        blur.style.backdropFilter = "blur(0px)";
+        blur.style.webkitBackdropFilter = "blur(0px)";
+        blur.style.MozBackdropFilter = "blur(0px)"; // Unsupported
+        blur.style.MsBackdropFilter = "blur(0px)"; // Unsupported
+        blur.style.OBackdropFilter = "blur(0px)"; // Unsupported
+      }
     });
-  }
-  showProgress();
-  // SKILL BARS END
+  });
   // VIDEO CONTROLS START
   const video = document.getElementById("heroVideo"),
     pauseControl = document.getElementById("pauseControl"),
@@ -229,51 +234,54 @@ if (document.body.id === "homepage") {
   }
 }
 // VIDEO OFFLOAD END
-
 // HOMEPAGE END
+
+// LOTTIE INTERACTIVITY START
+function createLottieInteractivity(playerElement) {
+  LottieInteractivity.create({
+    player: playerElement,
+    mode: "scroll",
+    actions: [
+      {
+        visibility: [0.1, 1.0],
+        type: "play",
+      },
+    ],
+  });
+}
+
+// Get all lottie-player elements and apply interactivity
+const lottiePlayers = document.querySelectorAll("lottie-player");
+
+lottiePlayers.forEach((player) => {
+  createLottieInteractivity(player);
+});
+
+// LOTTIE INTERACTIVITY END
+
 // CONTACT FORM START
-(function contactForm() {
+document.addEventListener("DOMContentLoaded", function () {
   const contactForm = document.getElementById("contactForm");
 
-  /// LOADER AT SUBMITING A FORM START
-  function onFormSubmission(event) {
-    const isValid = Array.from(event.target.elements).every((element) =>
+  if (contactForm) {
+    // LOADER AT SUBMITTING A FORM START
+    function onFormSubmission(event) {
+      const isValid = Array.from(event.target.elements).every((element) =>
         element.reportValidity()
-      ),
-      submitButton = document.querySelector(".submit-btn");
+      );
 
-    if (isValid) {
-      submitButton.innerHTML = "<div class='loader'></div>";
-    } else {
-      event.preventDefault();
+      const submitButton = document.querySelector(".submit-btn");
+
+      if (isValid) {
+        submitButton.innerHTML = "<div class='loader'></div>";
+      } else {
+        event.preventDefault();
+      }
     }
+
+    contactForm.addEventListener("submit", onFormSubmission);
+    // LOADER AT SUBMITTING A FORM END
   }
-  contactForm.addEventListener("submit", onFormSubmission);
-  // LOADER AT SUBMITING A FORM END
-
-  // SUBMIT AT ENTER START
-  document.addEventListener("keyup", function (event) {
-    if (event.keyCode === 13) {
-      onFormSubmission(event);
-    }
-  });
-  // SUBMIT AT ENTER END
-})();
-// CONTACT FORM END
-// RECAPTCHA START
-function onSubmit(token) {
-  document.getElementById("contactForm").submit();
-}
-// RECAPTCHA END
-
-// VIMEO GALLERY START
-$(document).ready(function () {
-  $(".video-gallery").magnificPopup({
-    delegate: "a",
-    type: "iframe",
-    gallery: {
-      enabled: true,
-    },
-  });
 });
-// VIMEO GALLERY END
+
+// CONTACT FORM END
