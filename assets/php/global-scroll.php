d6bafd103ab49e1a9da665a01eb790abd2412c93
Adam S <98590550+adamsochorec@users.noreply.github.com>
2023-11-21 15:09:13 +0100

    wtf changes

--- assets/php/global-scroll.php
+++
@@ -1,9 +0,0 @@
-<div id="wrapper" class="scroll" role="region" aria-label="Scroll Down Section">
-  <div id="wrapper-inner">
-    <div id="scroll-down" tabindex="0">
-      <span class="arrow-down" aria-hidden="true">
-        <!-- CSS generated icon for decorative purpose -->
-      </span>
-    </div>
-  </div>
-</div>
